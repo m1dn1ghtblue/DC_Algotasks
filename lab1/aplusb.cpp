dd6519c978395e65b0cb5ac7580c5324e0630c56
Ilya <kotyrov.1310@gmail.com>
2021-10-30 14:58:21 +0300

    added task 2

---
+++ lab1/aplusb.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+ 
+using namespace std;
+int main() {
+    freopen("aplusb.in", "r", stdin);
+    freopen("aplusb.out", "w", stdout);
+ 
+    long long a, b;
+    cin >> a >> b;
+    cout << a+b;
+ 
+    return 0;
+}
