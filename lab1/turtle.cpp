8039c13ae73f5ec4c26ee47078f7462fcd0e6383
inter-malchik <89922397+inter-malchik@users.noreply.github.com>
2021-11-04 15:44:21 +0300

    Update turtle.cpp
    Added spaces between binary operators, changed syntaxis

--- lab1/turtle.cpp
+++ lab1/turtle.cpp
@@ -5,7 +5,6 @@ using namespace std;
 int main() {
     freopen("turtle.in", "r", stdin);
     freopen("turtle.out", "w", stdout);
- 
     int h, w;
     cin >> h >> w;
     vector < vector<int> > field;
@@ -16,22 +15,17 @@ int main() {
             cin >> field[i][j];
         }
     }
- 
     for (int i = h-2; i >= 0; i--){
-        field[i][0] += field[i+1][0];
+        field[i][0] += field[i + 1][0];
     }
- 
     for (int i = 1; i < w; i++){
-        field[h-1][i] += field[h-1][i-1];
+        field[h - 1][i] += field[h - 1][i - 1];
     }
- 
-    for (int i = h-2; i >= 0; i --){
+    for (int i = h - 2; i >= 0; i--){
         for (int j = 1; j < w; j++){
-            field[i][j] += max(field[i+1][j], field[i][j-1]);
+            field[i][j] += max(field[i + 1][j], field[i][j - 1]);
         }
     }
- 
-    cout << field[0][w-1];
- 
+    cout << field[0][w - 1];
     return 0;
 }
