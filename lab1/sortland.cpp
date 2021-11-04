9f534200fa55ef6690bab8bc9641c1d2d6447c45
inter-malchik <89922397+inter-malchik@users.noreply.github.com>
2021-11-04 15:43:20 +0300

    Update sortland.cpp
    Added spaces between binary operators, changed syntaxis

--- lab1/sortland.cpp
+++ lab1/sortland.cpp
@@ -1,38 +1,32 @@
 #include <iostream>
 #include <vector>
- 
 using namespace std;
+
 int main() {
- 
     freopen("sortland.in", "r", stdin);
     freopen("sortland.out", "w", stdout);
- 
     int n;
     cin >> n;
- 
     vector<pair<float, int>> v;
     v.resize(n);
- 
     for (int i = 0; i < n; i++) {
         float x;
         cin >> x;
-        v[i] = make_pair(x, i+1);
+        v[i] = make_pair(x, i + 1);
     }
- 
     //sort
- 
     for (int i = 1; i < n; i++){
         float k = v[i].first;
-        for (int j = i-1; j >= 0; j--){
+        for (int j = i - 1; j >= 0; j--){
             if(k < v[j].first){
-                pair <float,int> tmp = v[j+1];
-                v[j+1] = v[j];
+                pair <float,int> tmp = v[j + 1];
+                v[j + 1] = v[j];
                 v[j] = tmp;
             }
             else break;
         }
     }
  
-    cout << v[0].second << " " << v[n/2].second << " " << v[n-1].second;
+    cout << v[0].second << " " << v[n / 2].second << " " << v[n - 1].second;
     return 0;
 }
